7d55971152bd4b43678ceed60785d16dea7dcabe
User10538 <10538@holbertonstudents.com>
2025-04-10 00:52:10 +0000

    attempt 2

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,49 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
+#include "sort.h"
+/**
+ * quick_sort - function that sorts an array of integers in
+ * ascending order using the Quick sort algo
+ *
+ * @array: int
+ * @size: size_t
+ */
 void quick_sort(int *array, size_t size)
 {
-    size_t low = 0, high = size - 1, i, j, temp, partition_index, k;
-    int pivot;
 
-    if (!array || size < 2)
-        return;
+        int low = 0, high = (int)(size - 1), pivot, i, j, temp, partition_index;
 
-    if (low < high)
-    {
-        pivot = array[high];
-        i = low - 1;
+        if (!array || size < 2)
+                return;
 
-        for (j = low; j < high; j++)
+        while (low < high)
         {
-            if (array[j] < pivot)
-            {
-                i++;
-                temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
+                pivot = array[high];
+                i = low - 1;
+
+                for (j = low; j < high; j++)
+                {
+                        if (array[j] < pivot)
+                        {
+                                i++;
+                                temp = array[i];
+                                array[i] = array[j];
+                                array[j] = temp;
+                                print_array(array, size);
+                        }
+                }
+                temp = array[i + 1];
+                array[i + 1] = array[high];
+                array[high] = temp;
+                print_array(array, size);
+
+                partition_index = i + 1;
+                if (partition_index - 1 > low)
+                        high = partition_index - 1;
+                else
+                low = partition_index + 1;
         }
 
-        temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
-
-        partition_index = i + 1;
-
-        for (k = 0; k < size; k++)
-        {
-            if (k != 0)
-                printf(", ");
-            printf("%d", array[k]);
-        }
-        printf("\n");
-
-        if (partition_index > low)
-            quick_sort(array, partition_index);
-        if (partition_index + 1 < high)
-            quick_sort(array + partition_index + 1, size - (partition_index + 1)); 
-    }
 }
-
