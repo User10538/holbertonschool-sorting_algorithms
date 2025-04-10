70de1a64847c0902be8f44b3ebc143f31073e643
User10538 <10538@holbertonstudents.com>
2025-04-10 12:13:54 +0000

    Fix Betty error

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,7 +10,7 @@
 void swap(int *a, int *b)
 {
 	int temp;
-	
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
@@ -26,21 +26,13 @@ void swap(int *a, int *b)
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-	int pivot, i, j;
-	int *last_printed_array;  /* Pointer for storing the last printed state */
-	int array_changed;
-       	/* Dynamically allocate memory for last_printed_array */
+	int pivot, i, j, *last_printed_array, array_changed;
+
 	last_printed_array = (int *)malloc(size * sizeof(int));
-	
 	if (last_printed_array == NULL)
-	{
-		return -1;
-	}
-	
+		return (-1);
 	pivot = array[high];
-       	i = low - 1;
-	
-	/* Copy the initial state of the array */
+	i = low - 1;
 	memcpy(last_printed_array, array, size * sizeof(int));
 	for (j = low; j < high; j++)
 	{
@@ -50,32 +42,28 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 			if (i != j)
 			{
 				swap(&array[i], &array[j]);
-				array_changed = memcmp(last_printed_array,
-						array, size * sizeof(int)); /* Check if array changed */
+				array_changed = memcmp(last_printed_array, array, size * sizeof(int));
 				if (array_changed != 0) /* If the array has changed, print it */
 				{
-					memcpy(last_printed_array, array, size * sizeof(int)); /* Update last printed state */
+					memcpy(last_printed_array, array, size * sizeof(int));
 					print_array(array, size);
 				}
 			}
 		}
 	}
 	if (i + 1 != high)
-    	{
+	{
 		swap(&array[i + 1], &array[high]);
-		array_changed = memcmp(last_printed_array, array, size * sizeof(int)); /* Check if array changed */
-        	
+		array_changed = memcmp(last_printed_array, array, size * sizeof(int));
+
 		if (array_changed != 0) /* If the array has changed, print it */
 		{
-			memcpy(last_printed_array, array, size * sizeof(int)); /* Update last printed state */
+			memcpy(last_printed_array, array, size * sizeof(int));
 			print_array(array, size);
-	   	}
+		}
 	}
-
-    /* Free the dynamically allocated memory */
-    free(last_printed_array);
-
-    return (i + 1);
+	free(last_printed_array);
+	return (i + 1);
 }
 
 /**
@@ -87,14 +75,14 @@ int lomuto_partition(int *array, int low, int high, size_t size)
  */
 void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
-    int pivot;
+	int pivot;
 
-    if (low < high)
-    {
-        pivot = lomuto_partition(array, low, high, size);
-        quick_sort_recursive(array, low, pivot - 1, size);
-        quick_sort_recursive(array, pivot + 1, high, size);
-    }
+	if (low < high)
+	{
+		pivot = lomuto_partition(array, low, high, size);
+		quick_sort_recursive(array, low, pivot - 1, size);
+		quick_sort_recursive(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -104,9 +92,8 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
-
-    quick_sort_recursive(array, 0, (int)(size - 1), size);
+	if (!array || size < 2)
+		return;
+	quick_sort_recursive(array, 0, (int)(size - 1), size);
 }
 
